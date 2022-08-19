2180bcb4de9d0eed3ac96ce334028f1112c991ec
Daiveed <jajadavidjid@gmail.com>
2022-08-20 00:13:12 +0100

    All pages built, functionality currently being added, still unresponsive

---
+++ index.js
@@ -0,0 +1,12 @@
+// import initScrollReveal from "./scripts/scrollReveal";
+// import initTiltEffect from "./scripts/tiltAnimation";
+// import { targetElements, defaultProps } from "./data/scrollRevealConfig";
+
+import initScrollReveal from "./Scripts/scrollReveal";
+
+import initTiltEffect from "./Scripts/tiltAnimation";
+
+import { targetElements, defaultProps } from "./Data/scrollRevealConfig";
+
+initScrollReveal(targetElements, defaultProps);
+initTiltEffect();
