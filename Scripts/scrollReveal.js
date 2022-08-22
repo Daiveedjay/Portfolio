04cec47006f3b62d449456c171eb1192e85bd1ce
Daiveed <jajadavidjid@gmail.com>
2022-08-22 18:24:53 +0100

    Media Queries in the works

--- Scripts/scrollReveal.js
+++ Scripts/scrollReveal.js
@@ -1,9 +1,79 @@
-// export default function initScrollReveal(targetElements, defaultProps) {
-//   if (!targetElements.length) return;
+"use strict";
 
-//   ScrollReveal({ reset: false });
+// Targets all sections
+const allSections = document.querySelectorAll("section");
 
-//   targetElements.forEach(({ element, animation }) => {
-//     ScrollReveal().reveal(element, Object.assign({}, defaultProps, animation));
-//   });
-// }
+// Target the project section
+const projectContainers = document.querySelectorAll(".project__container");
+
+const options = {
+  root: null,
+  threshold: 0.3,
+};
+
+const sectionObserver = new IntersectionObserver(callback, options);
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+});
+
+projectContainers.forEach((project) => {
+  sectionObserver.observe(project);
+});
+
+document.querySelectorAll(".left__column").forEach((column) => {
+  column.classList.add("hidden__left");
+});
+document.querySelectorAll(".right__column").forEach((column) => {
+  column.classList.add("hidden__right");
+});
+
+function callback(entries, observer) {
+  const [entry] = entries;
+
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+
+  //   Get the class name of the target section that came into view
+  const curSectionsName = entry.target.getAttribute("class");
+  console.log(curSectionsName);
+
+  // Callback funtion for about section
+  if (curSectionsName === "about__section") {
+    //   now target that specific section
+    const curSection = document.querySelector(`.${curSectionsName}`);
+    //   remove the hidden classes (DOM traversing)
+    curSection.lastElementChild.lastElementChild.firstElementChild.classList.remove(
+      "hidden__left"
+    );
+    curSection.lastElementChild.lastElementChild.lastElementChild.classList.remove(
+      "hidden__right"
+    );
+  }
+
+  // This marks the begining of the project section functionality
+  else if (entry.target.classList.contains("project__container")) {
+    entry.target.firstElementChild.classList.remove("hidden__left");
+
+    entry.target.lastElementChild.classList.remove("hidden__right");
+  }
+
+  if (curSectionsName === "cta__section") {
+    //   now target that specific section
+    const curSection = document.querySelector(`.${curSectionsName}`);
+    curSection.lastElementChild.lastElementChild.classList.remove(
+      "hidden__bottom"
+    );
+  }
+
+  if (curSectionsName === "footer__section") {
+    const curSection = document.querySelector(`.${curSectionsName}`);
+
+    curSection.lastElementChild.firstElementChild.classList.remove(
+      "hidden__bottom"
+    );
+
+    curSection.lastElementChild.firstElementChild.nextElementSibling.classList.remove(
+      "hidden__bottom"
+    );
+  }
+}
